0530ce10bde882eb280c7520f946d3aa5b88b210
Adrian Przychodzień <adrianprzychodzien@gmail.com>
2020-02-12 17:08:04 +0100

    WeightTodayFormik component with Formik input to add everydays weight, informations about your weight status in weightTracker screen and CircularProgress bars

---
+++ components/WeightTodayFormik.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native'
+
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import moment from 'moment'
+
+import Colors from '../utils/Colors'
+import { globalStyles } from '../utils/globalStyles'
+
+
+const validationSchema = yup.object({
+  dailyWeight: yup.string().matches(/^[0-9]{1,2}([,.][0-9]{1,2})?$/, { message: 'Only numbers' }).required('Weight is required'),
+})
+
+const WeightTodayFormik = ({ setDailyWeight, userData }) => {
+  const { dailyWeightArray } = userData
+
+  const today = moment().toISOString().slice(0, 10)
+  // const tomorrow = moment().add(1, 'days').toISOString().slice(0, 10)
+  const lastWeightData = dailyWeightArray[dailyWeightArray.length - 1].date
+  const theSameDay = moment(today).isSame(lastWeightData)
+
+  return (
+    <Formik initialValues={{
+      dailyWeight: theSameDay ? (userData.weight || '') : '',
+    }}
+      validationSchema={validationSchema}
+      onSubmit={values => {
+        setDailyWeight({
+          date: new Date().toISOString().slice(0, 10),
+          weight: values.dailyWeight
+        })
+      }}
+    >
+      {({ handleSubmit, handleChange, handleBlur, values, errors, touched }) => (
+        <View style={{ ...globalStyles.container, paddingVertical: 0 }} >
+          <Text style={globalStyles.header}>Weight today</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.inputContainer}>
+              <TextInput style={styles.input}
+                onChangeText={handleChange('dailyWeight')}
+                onBlur={handleBlur('dailyWeight')}
+                value={values.dailyWeight}
+                placeholder="dailyWeight (kg)"
+                keyboardType="numeric"
+              />
+              <Text style={styles.errorText}>{touched.dailyWeight && errors.dailyWeight}</Text>
+            </View>
+
+          </View>
+
+          <View style={styles.button}>
+            <Button
+              title='submit'
+              color={Colors.primary}
+              onPress={handleSubmit}
+            />
+          </View>
+        </View>
+      )}
+    </Formik>
+  )
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: '50%',
+    marginHorizontal: 15,
+    paddingTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 6,
+    width: 90,
+    padding: 10,
+    marginLeft: 10
+  },
+  button: {
+    paddingVertical: 15,
+    width: '80%'
+  },
+  errorText: {
+    fontSize: 12,
+    color: 'red',
+    textAlign: 'center'
+  }
+})
+
+export default WeightTodayFormik
--- screens/WeightTracker.js
+++ screens/WeightTracker.js
@@ -6,8 +6,10 @@ import { Formik } from 'formik'
 import * as yup from 'yup'
 import moment from 'moment'
 import DateTimePicker from '@react-native-community/datetimepicker'
+import ProgressCircle from 'react-native-progress-circle'
 
 import { diffDays, weightTrackerInfo, percentageProgress, HealthTips } from '../utils/equations'
+import WeightTodayFormik from '../components/WeightTodayFormik'
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faWeight, faBullseye } from '@fortawesome/free-solid-svg-icons'
@@ -20,8 +22,8 @@ import Colors from '../utils/Colors'
 import { globalStyles } from '../utils/globalStyles'
 
 const validationSchema = yup.object({
-  weight: yup.string().matches(/^[0-9]*$/, { message: 'Only numbers' }).required('Weight is required').min(2),
-  weightGoal: yup.string().matches(/^[0-9]*$/, { message: 'Only numbers' }).required('Weight is required').min(2)
+  weight: yup.string().matches(/^[0-9]{1,2}([,.][0-9]{1,2})?$/, { message: 'Only numbers' }).required('Weight is required').min(2),
+  weightGoal: yup.string().matches(/^[0-9]{1,2}([,.][0-9]{1,2})?$/, { message: 'Only numbers' }).required('Weight is required').min(2)
 })
 
 const WeightTracker = ({
@@ -38,23 +40,20 @@ const WeightTracker = ({
   const [show, setShow] = useState(false)
 
   const setDateTimePicker = date => {
-    // let _date = date.nativeEvent.timestamp
-    // let _dateFormat = new Date(_date)
     let time = moment(date.nativeEvent.timestamp).format('YYYY-MM-DD')
-    setDate(time)
     setShow(false)
+    setDate(time)
+    setFinishDate({
+      finish: time,
+      start: new Date().toISOString().slice(0, 10)
+    })
   }
 
   console.log(date)
 
   const { weightGoal, finish, dailyWeightArray } = userData
-  // const weightToday = dailyWeightArray.length ? dailyWeightArray[dailyWeightArray.length - 1].weight : null
-  // const weightYesterday = dailyWeightArray.length > 1 ? dailyWeightArray[dailyWeightArray.length - 2].weight : null
-
-  // if input in DatePicker is empty set finish date to 4 weeks from today
-  // let finishDate = (new Date(date).toISOString().slice(0, 10) === '1970-01-01')
-  //   ? (moment().startOf('today').add(3, 'weeks')._d).toISOString().slice(0, 10)
-  //   : new Date(date).toISOString().slice(0, 10)
+  const weightToday = dailyWeightArray.length ? dailyWeightArray[dailyWeightArray.length - 1].weight : null
+  const weightYesterday = dailyWeightArray.length > 1 ? dailyWeightArray[dailyWeightArray.length - 2].weight : null
 
   const [daysCompletionPercentage, kgCompletionPercentage] = percentageProgress(userData, diffDays)
   const healthTips = HealthTips(userData, diffDays)
@@ -86,91 +85,155 @@ const WeightTracker = ({
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <ScrollView>
         <View style={globalStyles.container}>
-          <Formik initialValues={{
-            weight: userData.weight || '',
-            weightGoal: userData.weightGoal || ''
-          }}
-            validationSchema={validationSchema}
-            onSubmit={values => {
-              setWeightData({
-                weight: values.weight,
-                weightGoal: values.weightGoal
-              })
-              setDailyWeight({
-                date: new Date().toISOString().slice(0, 10),
-                weight: values.weight,
-              })
-              setShow(true)
-            }}
-          >
-            {({ handleSubmit, handleChange, handleBlur, values, errors, touched }) => (
-              <View style={{ ...globalStyles.container, paddingVertical: 0 }} >
-                <Text style={globalStyles.header}>Add your data??</Text>
-                <View style={{ flexDirection: 'row' }}>
-                  <View style={styles.inputContainer}>
-                    <View style={styles.inputSubcontainer}>
-                      <View>
-                        <FontAwesomeIcon icon={faWeight} color={Colors.primary} size={36} />
+
+          {!finish ? (
+            <>
+              {/* Formik actual weight & weight goal */}
+              <Formik initialValues={{
+                weight: userData.weight || '',
+                weightGoal: userData.weightGoal || ''
+              }}
+                validationSchema={validationSchema}
+                onSubmit={values => {
+                  setWeightData({
+                    weight: values.weight,
+                    weightGoal: values.weightGoal
+                  })
+                  setDailyWeight({
+                    date: new Date().toISOString().slice(0, 10),
+                    weight: values.weight,
+                  })
+                  setShow(true)
+                }}
+              >
+                {({ handleSubmit, handleChange, handleBlur, values, errors, touched }) => (
+                  <View style={{ ...globalStyles.container, paddingVertical: 0 }} >
+                    <Text style={globalStyles.header}>Add your data??</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                      <View style={styles.inputContainer}>
+                        <View style={styles.inputSubcontainer}>
+                          <View>
+                            <FontAwesomeIcon icon={faWeight} color={Colors.primary} size={36} />
+                          </View>
+                          <TextInput style={styles.input}
+                            onChangeText={handleChange('weight')}
+                            onBlur={handleBlur('weight')}
+                            value={values.weight}
+                            placeholder="Weight (kg)"
+                            keyboardType="numeric"
+                          />
+                        </View>
+                        <Text style={styles.errorText}>{touched.weight && errors.weight}</Text>
                       </View>
-                      <TextInput style={styles.input}
-                        onChangeText={handleChange('weight')}
-                        onBlur={handleBlur('weight')}
-                        value={values.weight}
-                        placeholder="Weight (kg)"
-                        keyboardType="numeric"
+
+                      <View style={styles.inputContainer}>
+                        <View style={styles.inputSubcontainer}>
+                          <View>
+                            <FontAwesomeIcon icon={faBullseye} color={Colors.primary} size={36} />
+                          </View>
+                          <TextInput style={styles.input}
+                            onChangeText={handleChange('weightGoal')}
+                            onBlur={handleBlur('weightGoal')}
+                            value={values.weightGoal}
+                            placeholder="Goal (kg)"
+                            keyboardType="numeric"
+                          />
+                        </View>
+                        <Text style={styles.errorText}>{touched.weightGoal && errors.weightGoal}</Text>
+                      </View>
+                    </View>
+
+                    <View style={styles.button}>
+                      <Button
+                        title={weightGoal ? 'Change goal & set deadline' : 'Add goal & set dealdline'}
+                        color={Colors.primary}
+                        onPress={handleSubmit}
                       />
                     </View>
-                    <Text style={styles.errorText}>{touched.weight && errors.weight}</Text>
+
                   </View>
+                )}
+              </Formik>
 
-                  <View style={styles.inputContainer}>
-                    <View style={styles.inputSubcontainer}>
-                      <View>
-                        <FontAwesomeIcon icon={faBullseye} color={Colors.primary} size={36} />
-                      </View>
-                      <TextInput style={styles.input}
-                        onChangeText={handleChange('weightGoal')}
-                        onBlur={handleBlur('weightGoal')}
-                        value={values.weightGoal}
-                        placeholder="Goal (kg)"
-                        keyboardType="numeric"
-                      />
-                    </View>
-                    <Text style={styles.errorText}>{touched.weightGoal && errors.weightGoal}</Text>
-                  </View>
-                </View>
-
-                <View style={styles.button}>
-                  <Button
-                    title={weightGoal ? 'Change' : 'Add'}
-                    color={Colors.primary}
-                    onPress={handleSubmit}
-                  />
-                </View>
-
+              {/* Set finish date - DatePicker */}
+              <View>
+                <Text>
+                  {weightGoal && show &&
+                    <DateTimePicker
+                      value={new Date()}
+                      mode='date'
+                      display="calendar"
+                      onChange={setDateTimePicker}
+                    />}
+                </Text>
               </View>
-            )}
-          </Formik>
+            </>
+          ) : (
+              <>
+                {/* Set today`s weight */}
+                <WeightTodayFormik
+                  setDailyWeight={setDailyWeight}
+                  userData={userData}
+                />
 
-          {/* Set finish date - DatePicker */}
-          {weightGoal && <View>
-            {/* <View>
-              <Button onPress={() => setShow(true)} title="Show date picker!" />
-            </View> */}
-            {show && <DateTimePicker value={new Date(2020, 4, 4)}
-              mode='date'
-              display="calendar"
-              onChange={setDateTimePicker}
-            />}
-          </View>
-          }
+                <View>
+                  {weightToday === weightYesterday ? (
+                    <Text style={styles.text}>The same weight as yesterday</Text>
+                  ) : (
+                      dailyWeightArray.length >= 2 ? (
+                        <Text style={styles.text}>
+                          Actual weight {weightToday}kg, {(Math.abs(weightToday - weightYesterday)).toFixed(1)}kg
+                        {weightToday - weightYesterday < 0 ? ' less' : ' more'} than yesterday
+                      </Text>
+                      ) : (
+                          <Text style={styles.text}>Actual weight {weightToday}kg</Text>
+                        )
+                    )}
+                </View>
+
+                <Text style={styles.text}>
+                  {(diffDays(finish))} days left and {weightTrackerInfo(userData)}
+                </Text>
+
+                {/* Circular Progress */}
+                <View style={styles.circularProgressContainer}>
+                  <View style={styles.circularProgress}>
+                    <Text style={{ fontSize: 20 }}>Time progress</Text>
+                    <ProgressCircle
+                      percent={daysCompletionPercentage}
+                      radius={50}
+                      borderWidth={12}
+                      color={Colors.primary}
+                      shadowColor="#999"
+                      bgColor="#fff"
+                    >
+                      <Text style={{ fontSize: 18 }}>{`${daysCompletionPercentage}%`}</Text>
+                    </ProgressCircle>
+                  </View>
+
+                  <View style={styles.circularProgress}>
+                    <Text style={{ fontSize: 20 }}>Weight progress</Text>
+                    <ProgressCircle
+                      percent={kgCompletionPercentage}
+                      radius={50}
+                      borderWidth={12}
+                      color={Colors.primary}
+                      shadowColor="#999"
+                      bgColor="#fff"
+                    >
+                      <Text style={{ fontSize: 18 }}>{`${kgCompletionPercentage}%`}</Text>
+                    </ProgressCircle>
+                  </View>
+                </View>
+
+              </>
+            )}
+
         </View >
       </ScrollView>
     </TouchableWithoutFeedback>
   )
 }
-
-
 
 const styles = StyleSheet.create({
   inputContainer: {
@@ -196,12 +259,27 @@ const styles = StyleSheet.create({
   },
   button: {
     paddingVertical: 15,
-    width: '60%'
+    width: '80%'
   },
   errorText: {
     fontSize: 12,
     color: 'red',
     textAlign: 'center'
+  },
+  circularProgressContainer: {
+    marginVertical: 25,
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-evenly'
+  },
+  circularProgress: {
+    height: 140,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 22,
+    padding: 10
   }
 })
 
